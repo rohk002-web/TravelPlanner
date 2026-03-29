cc7ce410ca660fa9764742504c1dd82a11634831
rohith <rohk002@gmail.com>
2026-03-29 15:52:12 +0530

    minor changes in env and api

--- Frontend/src/api/Api.ts
+++ Frontend/src/api/Api.ts
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:8000";
+const API_BASE = import.meta.env.VITE_API_BASE;;
 
 export async function createUser(data: { email_id: string; password: string; name: string }) {
   const res = await fetch(`${API_BASE}/create-users`, {
