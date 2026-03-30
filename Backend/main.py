ff60ef48a3ca5424a8f6fa6c282c9276733827dc
rohith <rohk002@gmail.com>
2026-03-30 15:33:16 +0530

    cors orgin changed

--- Backend/main.py
+++ Backend/main.py
@@ -18,6 +18,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
+        "https://travelplanner-frontend.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
