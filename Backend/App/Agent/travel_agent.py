40805140ecfdfd06ffa428ab1b533d383f42234e
rohith <rohk002@gmail.com>
2026-04-17 23:48:35 +0530

    cache logic

--- Backend/App/Agent/travel_agent.py
+++ Backend/App/Agent/travel_agent.py
@@ -3,11 +3,18 @@ import json
 from dotenv import load_dotenv
 from google import genai
 from App.Agent.prompt import TRAVEL_AGENT_PROMPT
+from redis.asyncio import Redis
 
 load_dotenv()
+redis_client = Redis.from_url(os.getenv("REDIS_URL"))
 
 
 async def generate_travel_plan(data):
+    cache_key = f"travel_plan:{data.destination}:{data.travel_style}:{data.days}:{data.no_of_persons}:{data.budget_min}:{data.budget_max}"
+    cached_itinerary = await redis_client.get(cache_key)
+    if cached_itinerary:
+        print("---------------from cache ----------------")
+        return json.loads(cached_itinerary)
     client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
     prompt = TRAVEL_AGENT_PROMPT.format(
@@ -35,6 +42,7 @@ async def generate_travel_plan(data):
     # Parse as JSON
     try:
         itinerary = json.loads(text)
+        await redis_client.set(cache_key, json.dumps(itinerary), ex=3600)
         return itinerary
     except json.JSONDecodeError:
         # If parsing fails, return the cleaned text as error
